0ef9aef93bcf4683b814787332dcef9508ccd5ea
Punsano26 <xlichaphlreuxn@gmail.com>
2024-06-22 06:08:26 +0700

    Edit perfect version tailwindCSS Finished

---
+++ src/data/dataRestaurant.js
@@ -0,0 +1,38 @@
+export const restaurants = [
+    {
+      img:
+        "https://food-cms.grab.com/compressed_webp/merchants/3-C4EFGLCWLEMTN6/hero/15e8c356-c4a6-4995-af03-de75b945737a__store_cover__2024__05__04__10__28__06.webp",
+      title: "หมูสะเต๊ะป้าสม ประตูผี - สำราญราษฎร์",
+      type: "อร่อยเหาะ",
+    },
+    {
+      img:
+        "https://food-cms.grab.com/compressed_webp/merchants/3-C2JGVA3UA2B1NT/hero/2d2ff3906ac347f3ba5a15830beaf564_1628816783883544461.webp",
+      title: "ติ๋มนมสด ประตูผี - ถ.มหาไชย จ.นครปฐม",
+      type: "อร่อย หอม ชื่นใจ ละมุนปากถ้าไม่เชื่อให้มาลองชิมดูได้เลย",
+    },
+    {
+      img:
+        "https://food-cms.grab.com/compressed_webp/merchants/3-C4LFTVLFLYJKEJ/hero/48ff79ff1d7a48f09aa03b254b59e8ab_1694678639584600512.webp",
+      title: "Tamarin Tree Cafe - ซอยรามบุตรี",
+      type: "โจ๊กต้องต้ทให้เละถ้าต้มไม่เละมันไม่เป็นโจ๊ค",
+    },
+    {
+      img:
+        "https://food-cms.grab.com/compressed_webp/merchants/3-C6DJR3JJHELATA/hero/828cc275-7a51-4707-8539-085fb7315f92__store_cover__2024__02__02__13__09__49.webp",
+      title: "Sareena หม่าล่า (ฮาล้าล) - โรงเรียนสตรวิทยา",
+      type: "สำหรับคนชอบกินรสแซ่บ เจ้าของร้านก็แซ่บเหมือนกัน",
+    },
+    {
+      img:
+        "https://food-cms.grab.com/compressed_webp/merchants/3-C24XC3DCC8LECN/hero/b3f2e4d8e4e54c8ebef37f84475a837d_1636028237638835591.webp",
+      title: "คินุ โดนัท - ตะนาว",
+      type: "โดนัทยังมีรูแล้ว you มีใจไหม หอมหวานอร่อยสดใหม่ รสชาติไม่ซ้ำใคร เพราะไม่มีใครกล้าทำตาม ทางร้านรับประกันถ้าไม่อร่อยยินดีคืนเงิน",
+    },
+    {
+      img:
+        "https://food-cms.grab.com/compressed_webp/merchants/3-C6LXAE6AL651VJ/hero/fe14c75880734784b58ae514b16e59f8_1715584644922056005.webp",
+      title: "PAPAPALM JUICE & SMOOTHIES - ถนนตะนาว",
+      type: "น้ำสตอเบอเลิ่ม อร่อยหวานชื่นจาย ถ้าไม่อร่อยไปด่าร้านข้างๆได้เลย!",
+    },
+  ];
