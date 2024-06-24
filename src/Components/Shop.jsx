1ab54af012c05f61e3e643f6c2d4c99da48ef1da
punsano26 <xlichaphlreuxn@gmail.com>
2024-06-24 17:04:08 +0700

    Join server json

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Card } from "./Card";
 
-import { restaurants } from "../data/dataRestaurant";
+// import { restaurants } from "../data/dataRestaurant";
+
 const Shop = () => {
-  
+  const [restaurant, setRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/restaurant")
+      .then(
+        // convert เป็น jason
+        (res) => {
+          return res.json();
+        }
+      )
+      // เรียกใช้ State
+      .then((response) => {
+        setRestaurant(response);
+      })
+      // เปลี่ยนเป็น messsage
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <>
       <div className="flex flex-wrap justify-center gap-7">
-        {restaurants.map((restaurants) => {
-          return(<Card
-            img={restaurants.img}
-            title={restaurants.title}
-            type={restaurants.type}
-          />);
-        })}
-
+        {restaurant &&
+          restaurant.map((restaurants) => {
+            return (
+              <Card
+                key={restaurants.id}
+                img={restaurants.img}
+                title={restaurants.title}
+                type={restaurants.type}
+              />
+            );
+          })}
 
         {/* <Card img={restaurants[0].img} title={restaurants[0].title} type={restaurants[0].type} /> */}
 
@@ -23,8 +46,6 @@ const Shop = () => {
         <Card img={resto.img} title={resto.title} type={resto.type} />
         <Card img={resto.img} title={resto.title} type={resto.type} />
         <Card img={resto.img} title={resto.title} type={resto.type} /> */}
-
-
       </div>
     </>
   );
